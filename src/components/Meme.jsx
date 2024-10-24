878a13b73b191c7ef8a784bd8bcb4d3052d0d45d
mariomessina03 <171628699+mariomessina03@users.noreply.github.com>
2024-10-24 07:47:56 +0200

    Finalize Meme Generator

---
+++ src/components/Meme.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+export default function Meme() {
+  const [meme, setMeme] = React.useState({
+    topText: "È L'ORA ⌚ DELLO SBUSTO 🛍️",
+    bottomText: "È L'ORA ⌚ DEL PACCHETTO CHE SI APRE,",
+    randomImage: "https://i.imgflip.com/3po4m7.jpg",
+  });
+
+  const [allMemes, setAllMemes] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
+
+  function getMemeImage() {
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }
+
+  console.log(meme);
+
+  return (
+    <main>
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Top text"
+          className="form-input"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="form-input"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
+        <button onClick={getMemeImage} className="form-button">
+          Get a new meme image 🖼
+        </button>
+      </div>{" "}
+      <div className="meme">
+        <img src={meme.randomImage} className="meme-image" />
+        <h2 className="meme-text top">{meme.topText}</h2>
+        <h2 className="meme-text bottom">{meme.bottomText}</h2>
+      </div>
+    </main>
+  );
+}
